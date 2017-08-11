6a689f88dead85e3e8747f595464d587f1e79a4b
tihmstar <tihmstar@gmail.com>
2017-08-11 19:25:10 +0200

    Initial Commit

---
+++ nonceSetter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nonceSetter
+//
+//  Created by tihmstar on 11.08.17.
+//  Copyright © 2017 tihmstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nonceSetter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nonceSetter
+//
+//  Created by tihmstar on 11.08.17.
+//  Copyright © 2017 tihmstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
